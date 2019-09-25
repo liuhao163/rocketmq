5051e1eb48496131529a4bdfd51357fe1ae6cfa2
liuhaoeric <liuhaoeric@didiglobal.com>
2019-09-25 20:41:48 +0800

    MQClientInstance.java DefaultMQProducerImpl.java
    line 595

--- client/src/main/java/org/apache/rocketmq/client/consumer/rebalance/AllocateMessageQueueAveragely.java
+++ client/src/main/java/org/apache/rocketmq/client/consumer/rebalance/AllocateMessageQueueAveragely.java
@@ -51,11 +51,19 @@ public class AllocateMessageQueueAveragely implements AllocateMessageQueueStrate
             return result;
         }
 
+        //当前队列的下标
         int index = cidAll.indexOf(currentCID);
+        //mod代表多出来的队列数
         int mod = mqAll.size() % cidAll.size();
+        //averageSize 奇数
+        // mqAll.size() <= cidAll.size() 消费者数量超过了Mq队列永远返回1
+        // mod>0 && index < mod，说明：不能整除，index小于mod说明有富裕的mq可分配mqAll.size() / cidAll.size()+1，否则mqAll.size() / cidAll.size()
         int averageSize =
             mqAll.size() <= cidAll.size() ? 1 : (mod > 0 && index < mod ? mqAll.size() / cidAll.size()
                 + 1 : mqAll.size() / cidAll.size());
+
+        //startIndex：
+        //不能整除：并且有富裕的多出来的，index*avarageSize
         int startIndex = (mod > 0 && index < mod) ? index * averageSize : index * averageSize + mod;
         int range = Math.min(averageSize, mqAll.size() - startIndex);
         for (int i = 0; i < range; i++) {
@@ -67,5 +75,28 @@ public class AllocateMessageQueueAveragely implements AllocateMessageQueueStrate
     @Override
     public String getName() {
         return "AVG";
+    }
+
+    public static void main(String[] args) {
+        int index=2;
+        int mqSize=10;
+        int cId=7;
+        int mod=mqSize%cId;
+
+        int averageSize =
+                mqSize <= cId ? 1 : (mod > 0 && index < mod ? mqSize/ cId
+                        + 1 : mqSize / cId);
+        System.out.println(averageSize);
+        int startIndex = (mod > 0 && index < mod) ? index * averageSize : index * averageSize + mod;
+        System.out.println(startIndex);
+
+        int range = Math.min(averageSize, mqSize - startIndex);
+        System.out.println(range);
+
+        System.out.println("=====");
+        for (int i = 0; i < range; i++) {
+            System.out.println((startIndex + i) % mqSize);
+        }
+
     }
 }
